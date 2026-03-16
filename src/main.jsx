b2193d60180150e8bff4077349c583aeab179a8d
motebaya <davinsexo722@gmail.com>
2026-03-16 16:44:39 +0700

    fix: wrong base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './context/ThemeContext.jsx'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/imagegen">
+    <BrowserRouter basename="/cimagen/">
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </BrowserRouter>
   </StrictMode>,
-)
+);
