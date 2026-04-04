f46311b56e5f907490b0d68c382861f98303506b
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:45 +0700

    refactor(background-removal): add background removal utility

---
+++ src/utils/backgroundRemoval.js
@@ -0,0 +1,13 @@
+export {
+  BACKGROUND_MODE_OPTIONS,
+  BACKGROUND_REMOVAL_TIERS,
+  DEFAULT_BACKGROUND_REMOVAL_SETTINGS,
+  EXPORT_FORMATS,
+  getBackgroundRemovalModelById,
+  getBackgroundRemovalTiers,
+  getDefaultBackgroundRemovalModel,
+  getDefaultTier,
+  getModelsForTier,
+  loadBackgroundRemovalModel,
+  removeBackground,
+} from "./background-remover/index.js";
