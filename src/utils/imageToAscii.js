a051cb7c9ae05c255a16be251088ba206071330b
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:51 +0700

    refactor(image-to-ascii): add image to ascii utility

---
+++ src/utils/imageToAscii.js
@@ -0,0 +1,8 @@
+export {
+  CONVERSION_CHARACTERS,
+  DEFAULT_SORTED_CHARSET,
+  getBrightnessOfChar,
+  imageToAscii,
+  renderAsciiToCanvas,
+  sortCharsetByBrightness,
+} from "./image-to-ascii/index.js";
