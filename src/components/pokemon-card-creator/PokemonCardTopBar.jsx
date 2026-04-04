903dc6921fb9eea95a267a019783983cfb844541
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:36 +0700

    refactor(pokemon-card-creator): add pokemon card top bar component

---
+++ src/components/pokemon-card-creator/PokemonCardTopBar.jsx
@@ -0,0 +1,6 @@
+import HistoryPanel from "../HistoryPanel.jsx";
+import CreatorTopBar from "../creator/CreatorTopBar.jsx";
+
+export default function PokemonCardTopBar({ historyPanel }) {
+  return <CreatorTopBar right={<HistoryPanel {...historyPanel} />} />;
+}
