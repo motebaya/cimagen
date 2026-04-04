76a8c2a7637af5baa0d405b50208f3a8ccadcf21
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:54 +0700

    refactor(pixel-converter): add pixel converter utility

---
+++ src/utils/pixelConverter.js
@@ -0,0 +1,10 @@
+export {
+  COLOR_COUNT_OPTIONS,
+  OUTPUT_SCALE_OPTIONS,
+  PALETTE_PRESETS,
+  PIXEL_SIZE_OPTIONS,
+  createPixelArt,
+  createPixelJson,
+  createPixelSvg,
+  renderPixelArtCanvas,
+} from "./image-to-pixel/index.js";
