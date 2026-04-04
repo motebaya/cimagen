be694089d666e2803ce61d82a78f9cdf89ed846d
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:53 +0700

    refactor(metadata-viewer): add index utility

---
+++ src/utils/metadata-viewer/index.js
@@ -0,0 +1,17 @@
+export {
+  METADATA_METHOD_OPTIONS,
+  getMetadataMethodOption,
+} from "./metadataMethods.js";
+export {
+  METADATA_IMAGE_ACCEPT_ATTRIBUTE,
+  buildBasicMetadataItems,
+  buildExportMetadata,
+  buildPreviewBadges,
+} from "./metadataFormatting.js";
+export { normalizeMetadataTree } from "./metadataNormalization.js";
+export {
+  buildMetadataTreeText,
+  downloadCleanImage,
+  downloadMetadataJson,
+  downloadMetadataText,
+} from "./metadataExport.js";
