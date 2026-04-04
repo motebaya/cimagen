9dfa5df64f928c140afc80091f635cc27bf3a042
motebaya <davinsexo722@gmail.com>
2026-04-05 04:14:52 +0700

    refactor(meme-generator): add index utility

---
+++ src/utils/meme-generator/index.js
@@ -0,0 +1,7 @@
+export { MEME_FONTS } from "./memeConstants.js";
+export {
+  createDefaultTextLayer,
+  createImageLayer,
+  createStickerLayer,
+} from "./layerFactories.js";
+export { renderMeme } from "./renderer.js";
